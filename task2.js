66d908ac9677294ec089ebd93233cb647e285297
vijipriya110 <124969871+vijipriya110@users.noreply.github.com>
2023-06-10 18:54:13 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,395 @@
+
+db.zenclass.insertMany([
+    {
+        "date": "2020-10-01",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "CSS",
+        "tasks": "STYLE BY CONTENT",
+        "company_drives": "Elsevier",
+        "mentorscount": 15
+
+    },
+    {
+        "date": "2020-10-02",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "java script",
+        "tasks": "java script",
+        "company_drives": "Get My Parking",
+        "mentorscount": 15
+
+    },
+    {
+
+        "date": "2020-10-03",
+        "users": 50,
+        "codekata": 10,
+        "attandance": 100,
+        "topics": "DSI",
+        "tasks": "problem solved",
+        "company_drives": "Ecom Express",
+        "mentorscount": 15
+
+    },
+    {
+
+        "date": "2020-10-04",
+        "users": 0,
+        "codekata": 0,
+        "attandance": 0,
+        "topics": "my-sql",
+        "tasks": "create db by my-sql",
+        "company_drives": "Portea Medical",
+        "mentorscount": 16
+
+    },
+    {
+
+        "date": "2020-10-05",
+        "users": 5,
+        "codekata": 5,
+        "attadance": 5,
+        "topics": "datatypes",
+        "tasks": "problem solved",
+        "company_drives": "appeared",
+        "mentorscount": 14
+
+
+    },
+    {
+
+        "date": "2020-10-06",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "object",
+        "tasks": "problem solved",
+        "company_drives": "Square Yards",
+        "mentorscount": 14
+
+    },
+
+    {
+
+        "date": "2020-10-07",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "fsd demo",
+        "tasks": "fsd demo",
+        "company_drives": "Wipro",
+        "mentorscount": 16
+
+    },
+    {
+
+        "date": "2020-10-08",
+        "users": 5,
+        "codekata": 5,
+        "attandance": 5,
+        "topics": "node",
+        "tasks": "Bsics of node",
+        "company_drives": "Stashfin",
+        "mentorscount": 15
+
+    },
+    {
+
+        "date": "2020-10-09",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "react",
+        "tasks": "problem solved",
+        "company_drives": "HealthifyMe",
+        "mentorscount": 15
+
+    },
+    {
+
+        "date": "2020-10-10",
+        "users": 0,
+        "codekata": 0,
+        "attandance": 0,
+        "topics": "react js",
+        "tasks": "ract concepts",
+        "company_drives": "ReNew Power",
+        "mentorscount": 13
+
+    },
+    {
+
+
+        "date": "2020-10-11",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "Arithimatic operations",
+        "tasks": "problem solved ",
+        "company_drives": "Quikr",
+        "mentorscount": 17
+
+    },
+    {
+
+        "date": "2020-10-12",
+        "users": 5,
+        "codekata": 5,
+        "attandance": 5,
+        "topics": "String",
+        "tasks": "problem solved",
+        "company_drives": "Fujitsu",
+        "mentorscount": 17
+
+    },
+    {
+
+        "date": "2020-10-13",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "Array",
+        "tasks": "problems solved",
+        "company_drives": "Polygon",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-14",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "Function",
+        "tasks": "Problem solved",
+        "company_drives": "Ernst & Young",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date":"2020-10-15",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "node",
+        "tasks": "basics of node",
+        "company_drives": "QuickSell",
+        "mentorscount": 18
+
+    },
+    {
+
+        "date": "2020-10-16",
+        "users": 0,
+        "codekata": 0,
+        "attandance": 0,
+        "topics": "basic problem solving",
+        "tasks": " basic problem solved",
+        "company_drives": "Genpact",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-17",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "HTML",
+        "tasks": 0,
+        "company_drives": "L&T",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-18",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "class",
+        "tasks": "problem solved",
+        "company_drives": "Hella",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-19",
+        "users": 10,
+        "codekata": 10,
+        "attadance": 10,
+        "topics": "logical problems",
+        "tasks": "basic logical problems",
+        "company_drives": "Infrrd",
+        "mentorscount": 16
+
+    },
+    {
+
+        "date": "2020-10-20",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "HTML",
+        "tasks": "basic code of html",
+        "company_drives": "PwC",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-21",
+        "users": 0,
+        "codekata": 0,
+        "attandance": 0,
+        "topics": "bootstrap",
+        "tasks": "basic bootstrap",
+        "company_drives": "IndiaMART",
+        "mentorscount": 15
+
+    },
+    {
+
+        "date": "2020-10-22",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "CSS",
+        "tasks": "basic of css",
+        "company_drives": "Siemens",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-23",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "java script",
+        "tasks": "bsic of java script",
+        "company_drives": "Konecranes",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-24",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "DSI",
+        "tasks": "problem solved",
+        "company_drives": "Verint Systems",
+        "mentorscount": 17
+
+    },
+    {
+
+        "date": "2020-10-25",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "my-sql",
+        "tasks": 0,
+        "company_drives": "Shell",
+        "mentorscount": 15
+
+    },
+    {
+
+        "date": "2020-10-26",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "datatypes",
+        "tasks": "problem solved",
+        "company_drives": "Kuehne Nagel",
+        "mentorscount": 16
+
+    },
+    {
+
+        "date": "2020-10-27",
+        "users": 0,
+        "codekata": 0,
+        "attandance": 0,
+        "topics": "object",
+        "tasks": 0,
+        "company_drives": "Greenko Group",
+        "mentorscount": 17
+
+    },
+    {
+
+        "date": "2020-10-28",
+        "users": 12,
+        "codekata": 12,
+        "attandance": 12,
+        "topics": "class",
+        "tasks": "basics about class",
+        "company_drives": "JSW Steel",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-29",
+        "users": 5,
+        "codekata": 5,
+        "attandance": 5,
+        "topics": "fsd demo",
+        "tasks": "fsd demo",
+        "company_drives": "DXC Technology",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-30",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "node",
+        "tasks": "About node",
+        "company_drives": "Amdocs",
+        "mentorscount": 14
+
+    },
+    {
+
+        "date": "2020-10-31",
+        "users": 10,
+        "codekata": 10,
+        "attandance": 10,
+        "topics": "react",
+        "tasks": 0,
+        "company_drives": "guvi",
+        "mentorscount": 14
+
+
+    }
+])
+
+db.zenclass.find({}).toArray()
+
+
+1. db.zenclass.find().forEach(function(zen){print("Tasks :", zen.tasks, "Topics :", zen.topics)})
+
+2. db.zenclass.aggregate([{$match:{date:{$gte:"2020-10-15"}}},{$group:{_id:"$company_drives"}}])
+
+3. db.zenclass.aggregate([{$match:{users:{$gt:0}}},{$group:{_id:"$company_drives"}}])
+
+4. db.zenclass.aggregate([{$group:{ _id:"$codekata", totalQuantity:{$sum:"$codekata"}}}])
+
+5. db.zenclass.find({mentorscount:{$gte:15}})
+
+6. db.zenclass.find({$or:[{attandance:{e:0}},{tasks:{e:0}}]})
+
+
+
